34b1140e07a5a76e8de942d982e8dd720e5b7c3e
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-02-08 21:30:43 +0300

    add try catch block

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,33 @@ import http from 'node:http';
 import { userDelete, userGet, userPost, userPut } from './routes/userRoutes';
 
 const server = http.createServer((request, response) => {
-  switch (request.method) {
-    case 'GET':
-      userGet(request, response);
-      break;
+  try {
+    switch (request.method) {
+      case 'GET':
+        userGet(request, response);
+        break;
 
-    case 'POST':
-      userPost(request, response);
-      break;
+      case 'POST':
+        userPost(request, response);
+        break;
 
-    case 'PUT':
-      userPut(request, response);
-      break;
+      case 'PUT':
+        userPut(request, response);
+        break;
 
-    case 'DELETE':
-      userDelete(request, response);
-      break;
+      case 'DELETE':
+        userDelete(request, response);
+        break;
 
-    default:
-      response.statusCode = 400;
-      response.write('No Response');
-      response.end();
+      default:
+        response.statusCode = 400;
+        response.write('No Response');
+        response.end();
+    }
+  } catch (err) {
+    response.statusCode = 500;
+    response.write(`Internal Server Error: ${(err as Error).message}`);
+    response.end();
   }
 });
 
